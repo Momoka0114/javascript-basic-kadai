476f26dc71333481cca997bfb62dc2b031d007b5
Momoka0114 <momoka010123@gmail.com>
2026-03-05 00:07:01 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,12 +1,11 @@
-console.log('体重：',68,'kg');
-console.log('身長：',1.7,'m');
-
+//変数の宣言・値の代入
 let bodyWeight =68;
 
-let height = 170;
+//変数の宣言・値の代入
+let height = 1.7;
 
 //変数の宣言・値の代入
-let bmi = 68 / 170 * 170 ;
+let bmi = bodyWeight / height * height ;
 
 //コンソールへの出力
 console.log(bmi);
