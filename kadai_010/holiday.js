079d8ff97aef43a9e4f23d865e1a34e909619774
Momoka0114 <momoka010123@gmail.com>
2026-03-16 23:08:23 +0900

    Add files via upload

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -1,11 +1,11 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 
-// for文の場合:numの値が0以外である間、変数holidaysの値を出力する
-for (holodays) {
-  console.log(holidays);
+// for文の場合:0から1づつ増やし、holidaysの要素数分だけ取り出す
+for (let i = 1 ; i <= 17 ; i + 1) {
+  console.log(holidays[i]);
 }
 
-// while文の場合:numの値が0以外である間、変数holidaysの値を出力する
-while (holodays) {
-  console.log(holodays)
+// while文の場合:要素数が17未満の場合holidaysの要素数分だけ取り出す
+while (holidays < 17 ) {
+  console.log(holidays[i])
 }
