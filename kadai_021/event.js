96a15b0ff785de23a4ee8067c2559fe551dfbf2b
Momoka0114 <momoka010123@gmail.com>
2026-03-24 23:27:56 +0900

    Add files via upload

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -2,17 +2,9 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 //2秒の待ち時間を設定し、非同期処理を実行する
+btn.addEventListener('click', () => {
 setTimeout(() => {
   //取得した文字列にボタンをクリックしましたを出力する
   console.log('ボタンをクリックしました');
   text.textContent = 'ボタンをクリックしました';
-}, 2000);
-
-
-
-// HTML要素がクリックされたときにイベント処理を実行する
-btn.addEventListener('click', () => {
-  //取得した文字列にボタンをクリックしましたを出力する
-  console.log('ボタンをクリックしました');
-  text.textContent = 'ボタンをクリックしました';
-});
+}, 2000)});
